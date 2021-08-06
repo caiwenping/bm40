cd95a13377a7b99b10daa26c6edd65a5eba0186e
粑粑无礼头 <9602326+caiwenping@users.noreply.github.com>
2021-08-07 00:22:22 +0800

    Add files via upload

---
+++ config.h
@@ -0,0 +1,121 @@
+/* Copyright 2020 tominabox1
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x4B50 // "KP"
+#define PRODUCT_ID      0x3430 // "40"
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    KPrepublic
+#define PRODUCT         BM40 RGB 40%
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { B3, B2, E6, B5 }
+#define MATRIX_COL_PINS { B6, C6, B4, D7, D4, D6, C7, F6, F5, F4, F1, F0 }
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+//#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+//#define LOCKING_RESYNC_ENABLE
+
+#define RGB_DI_PIN E2
+#define DRIVER_LED_TOTAL 53
+#ifdef RGB_DI_PIN
+    #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+    #define RGB_MATRIX_KEYPRESSES
+    #define VIALRGB_NO_DIRECT
+
+    //#define DISABLE_RGB_MATRIX_ALPHAS_MODS
+    #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+    #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+    //#define DISABLE_RGB_MATRIX_BAND_SAT
+    #define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+    #define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+    #define DISABLE_RGB_MATRIX_BAND_VAL
+    #define DISABLE_RGB_MATRIX_BREATHING
+    #define DISABLE_RGB_MATRIX_CYCLE_ALL
+    //#define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+    #define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
+    #define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+    #define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
+    #define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
+    #define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
+    #define DISABLE_RGB_MATRIX_DIGITAL_RAIN
+    #define DISABLE_RGB_MATRIX_DUAL_BEACON
+    #define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+    #define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+    #define DISABLE_RGB_MATRIX_HUE_BREATHING
+    #define DISABLE_RGB_MATRIX_HUE_PENDULUM
+    #define DISABLE_RGB_MATRIX_HUE_WAVE
+    #define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+    #define DISABLE_RGB_MATRIX_MULTISPLASH
+    #define DISABLE_RGB_MATRIX_RAINBOW_BEACON
+    #define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+    #define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+    //#define DISABLE_RGB_MATRIX_RAINDROPS
+    #define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH
+    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+    //#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+    #define DISABLE_RGB_MATRIX_SOLID_SPLASH
+    #define DISABLE_RGB_MATRIX_SPLASH
+    //#define DISABLE_RGB_MATRIX_TYPING_HEATMAP#endif
+#endif
+
+#ifndef RGB_DISABLE_WHEN_USB_SUSPENDED
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
+#endif
+
+/* Configuration for Vial */
+#define VIAL_KEYBOARD_UID {0x6A, 0x02, 0xE7, 0x64, 0x1D, 0x59, 0x0D, 0xC8}
+
+// to unlock the keyboard
+//#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+//#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
+
+
+#define TAPPING_TOGGLE 3
+#define NO_AUTO_SHIFT_ALPHA
+
+
+#define MOUSEKEY_DELAY 50
+#define MOUSEKEY_INTERVAL 30            //Time between cursor movements in milliseconds
+#define MOUSEKEY_MOVE_DELTA 1           //Step size
+#define MOUSEKEY_MAX_SPEED 30           //Maximum cursor speed at which acceleration stops
+#define MOUSEKEY_TIME_TO_MAX 190         //Time until maximum cursor speed is reached
+#define MOUSEKEY_WHEEL_DELAY 100        //Delay between pressing a wheel key and wheel movement
+#define MOUSEKEY_WHEEL_INTERVAL 100     //Time between wheel movements
+#define MOUSEKEY_WHEEL_MAX_SPEED 8      //Maximum number of scroll steps per scroll action
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 50   //Time until maximum scroll speed is reached
---
+++ keymaps/vial/keymap.c
@@ -0,0 +1,129 @@
+/* Copyright 2020 tominabox1
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Layer 0 Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * | ESC  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | TAB  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl |  GUI |  Alt |MO(3) |MO(2) |    Space    |MO(1) |   /  | Left | Down |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[0] = LAYOUT_planck_mit(
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT ,
+    KC_LCTL, KC_LGUI, KC_LALT, MO(3),   MO(2),        KC_SPC,      MO(1),   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
+),
+
+/* Layer 1 12345
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Del  |C-Home|C-End |C-PgUp|C-PgDn|      |      |   -  |   =  |   [  |   ]  |  \   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |Shift |Gui-Lt|Gui-Rt|      |      |      |      |      |      |      | PgUp |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl |  GUI |  Alt |      |      |             |      |Insert| Home | PgDn | End  |
+ * `-----------------------------------------------------------------------------------'
+ */
+[1] = LAYOUT_planck_mit(
+    KC_GRV,  KC_1,          KC_2,         KC_3,          KC_4,             KC_5,    KC_6,   KC_7,     KC_8,       KC_9,    KC_0,      KC_BSPC,
+    KC_DEL,  LCTL(KC_HOME), LCTL(KC_END), LCTL(KC_PGUP), LCTL(KC_PGDOWN),  KC_NO,   KC_NO,  KC_MINS,  KC_EQL,     KC_LBRC, KC_RBRC,   KC_BSLS,
+    _______, 0x950,         0x94f,        KC_NO,         KC_NO,            KC_NO,   KC_NO,  KC_NO,    _______,    _______, KC_PGUP,   _______,
+    _______, _______,       _______,      _______,       _______,              _______,     _______,  KC_INSERT,  KC_HOME, KC_PGDOWN, KC_END
+),
+
+/* Layer 2 !@#$%
+ * ,-----------------------------------------------------------------------------------.
+ * |   ~  |   !  |   @  |   #  |  $   |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Del  |      |      |      |      |      |      |   _  |   +  |   {  |   }  |  |   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |Shift |      |      |      |      |      |      |      |      | Play | Vol+ | Mute |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl |  GUI |  Alt |      |      |             |      |      | Bri- | Vol- | Bri+ |
+ * `-----------------------------------------------------------------------------------'
+ */
+[2] = LAYOUT_planck_mit(
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+    _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+    _______, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MPLY, KC_VOLU, KC_MUTE,
+    _______, _______, _______, _______, _______,      _______,     _______, _______, KC_BRID, KC_VOLD, KC_BRIU
+),
+
+/* Layer 3 Function & Mouse
+ * ,-----------------------------------------------------------------------------------.
+ * | Task |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |C-A-D |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Caps |  F11 |  F12 |      |      |      |      |      |      |Mice-4|Mice-3|Mice-5|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |Shift |  PrS |  ScL |Pause |      |      |      |      |MiceWU|Mice-1|Mice-U|Mice-2|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl |  GUI |  Alt |      |      |    MO(4)    |TG(5) |MiceWD|Mice-L|Mice-D|Mice-R|
+ * `-----------------------------------------------------------------------------------'
+ */
+[3] = LAYOUT_planck_mit(
+    RCS(KC_ESCAPE), KC_F1,      KC_F2,         KC_F3,    KC_F4,   KC_F5,   KC_F6,  KC_F7,    KC_F8,    KC_F9,   KC_F10,  LCA(KC_DELETE),
+    KC_CAPSLOCK,    KC_F11,     KC_F12,        KC_NO,    KC_NO,   KC_NO,   KC_NO,  KC_NO,    KC_NO,    KC_BTN4, KC_BTN3, KC_BTN5,
+    _______,        KC_PSCREEN, KC_SCROLLLOCK, KC_PAUSE, KC_NO,   KC_NO,   KC_NO,  KC_NO  ,  KC_WH_U,  KC_BTN1, KC_MS_U, KC_BTN2,
+    _______,        _______,    _______,       _______,  _______,       MO(4),     TG(5)  ,  KC_WH_D,  KC_MS_L, KC_MS_D, KC_MS_R
+),
+
+/* Layer 4 System
+ * ,-----------------------------------------------------------------------------------.
+ * |      | Eft- | Eft+ |      |      |      |      |      |      |      |      | Reset|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      | Hue- | Hue+ |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |Apple |RGBTOG|Mode+ | Bri- | Bri+ |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[4] = LAYOUT_planck_mit(
+    KC_NO,                RGB_SPD,  RGB_SPI,  KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,  RESET,
+    KC_NO,                RGB_HUD,  RGB_HUI,  RGB_SAD,  RGB_SAI, KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,  KC_NO,
+    MAGIC_TOGGLE_ALT_GUI, RGB_TOG,  RGB_MOD,  RGB_VAD,  RGB_VAI, KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,  KC_NO,
+    KC_NO,                KC_NO,    KC_NO,    KC_NO,    KC_NO,         KC_NO,      KC_NO,    KC_NO,    KC_NO,   KC_NO,  KC_NO
+),
+
+/* Layer 5 Numberic
+ * ,-----------------------------------------------------------------------------------.
+ * |TG(5) |      |      |      |      |   7  |   8  |   9  |   /  |      |      | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |   4  |   5  |   6  |   *  |      |      | Del  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |   1  |   2  |   3  |   -  |      |  Up  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      0      |  .   |   +  | Left | Down |Right |
+ * `-----------------------------------------------------------------------------------'
+ */
+[5] = LAYOUT_planck_mit(
+    TG(5),  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_7,   KC_8,   KC_9,   KC_KP_SLASH,     KC_NO,   KC_NO,   KC_BSPC,
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_4,   KC_5,   KC_6,   KC_KP_ASTERISK,  KC_NO,   KC_NO,   KC_DEL,
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_1,   KC_2,   KC_3,   KC_KP_MINUS,     KC_NO,   KC_UP,   KC_ENT ,
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_0,      KC_DOT, KC_KP_PLUS,      KC_LEFT, KC_DOWN, KC_RGHT
+)
+
+};
+
