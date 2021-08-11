cb530f3fe55338cb3f3e14885d1c58fdd488d994
粑粑无礼头 <9602326+caiwenping@users.noreply.github.com>
2021-08-11 13:46:14 +0800

    Add files via upload

--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -108,20 +108,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Layer 5 Numberic
  * ,-----------------------------------------------------------------------------------.
- * |TG(5) |      |      |      |      |   7  |   8  |   9  |   -  |   /  |      | Bksp |
+ * | ESC  |   Q  |   W  |   E  |   R  |   7  |   8  |   9  |   -  |   /  |      | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |   4  |   5  |   6  |   +  |   *  |      | Del  |
+ * | Del  |   A  |   S  |   D  |   F  |   4  |   5  |   6  |   +  |   *  |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |   1  |   2  |   3  |   =  |      |      |Enter |
+ * | Shift|   Z  |   X  |   C  |   V  |   1  |   2  |   3  |   =  |      |      |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      0      |   .  | Left | Down |  Up  |Right |
+ * | Ctrl |  GUI |  Alt | TG(5)|      |      0      |   .  | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [5] = LAYOUT_planck_mit(
-    TG(5),  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_7,   KC_8,   KC_9,    KC_KP_MINUS,  KC_KP_SLASH,     KC_NO,   KC_BSPC,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_4,   KC_5,   KC_6,    KC_KP_PLUS,   KC_KP_ASTERISK,  KC_NO,   KC_DEL,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_1,   KC_2,   KC_3,    KC_EQL,       KC_NO,           KC_NO,   KC_ENT ,
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,      KC_0,      KC_DOT,  KC_LEFT,      KC_DOWN,         KC_UP,   KC_RGHT
+    KC_ESC,   KC_Q,      KC_W,  KC_E,    KC_R,  KC_7,   KC_8,   KC_9,    KC_KP_MINUS,  KC_KP_SLASH,     KC_NO,   KC_BSPC,
+    KC_DEL,   KC_A,      KC_S,  KC_D,    KC_F,  KC_4,   KC_5,   KC_6,    KC_KP_PLUS,   KC_KP_ASTERISK,  KC_NO,   KC_NO,
+    KC_LSFT,  KC_Z,      KC_X,  KC_C,    KC_V,  KC_1,   KC_2,   KC_3,    KC_EQL,       KC_NO,           KC_NO,   KC_ENT ,
+    KC_LCTL,  KC_LGUI,   KC_NO,  TG(5),  KC_NO,      KC_0,      KC_DOT,  KC_LEFT,      KC_DOWN,         KC_UP,   KC_RGHT
 )
 
 };
