7839caafe3f120fa51dbeef4e98e7a22d7bd651e
粑粑无礼头 <9602326+caiwenping@users.noreply.github.com>
2021-08-11 14:36:08 +0800

    Add files via upload

--- keymaps/vial/keymap.c
+++ keymaps/vial/keymap.c
@@ -118,10 +118,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [5] = LAYOUT_planck_mit(
-    KC_ESC,   KC_Q,      KC_W,  KC_E,    KC_R,  KC_7,   KC_8,   KC_9,    KC_KP_MINUS,  KC_KP_SLASH,     KC_NO,   KC_BSPC,
-    KC_DEL,   KC_A,      KC_S,  KC_D,    KC_F,  KC_4,   KC_5,   KC_6,    KC_KP_PLUS,   KC_KP_ASTERISK,  KC_NO,   KC_NO,
-    KC_LSFT,  KC_Z,      KC_X,  KC_C,    KC_V,  KC_1,   KC_2,   KC_3,    KC_EQL,       KC_NO,           KC_NO,   KC_ENT ,
-    KC_LCTL,  KC_LGUI,   KC_NO,  TG(5),  KC_NO,      KC_0,      KC_DOT,  KC_LEFT,      KC_DOWN,         KC_UP,   KC_RGHT
+    KC_ESC,   KC_Q,      KC_W,    KC_E,    KC_R,  KC_7,   KC_8,   KC_9,    KC_KP_MINUS,  KC_KP_SLASH,     KC_NO,   KC_BSPC,
+    KC_DEL,   KC_A,      KC_S,    KC_D,    KC_F,  KC_4,   KC_5,   KC_6,    KC_KP_PLUS,   KC_KP_ASTERISK,  KC_NO,   KC_NO,
+    KC_LSFT,  KC_Z,      KC_X,    KC_C,    KC_V,  KC_1,   KC_2,   KC_3,    KC_EQL,       KC_NO,           KC_NO,   KC_ENT ,
+    KC_LCTL,  KC_LGUI,   KC_LALT, TG(5),  KC_NO,      KC_0,      KC_DOT,  KC_LEFT,      KC_DOWN,         KC_UP,   KC_RGHT
 )
 
 };
